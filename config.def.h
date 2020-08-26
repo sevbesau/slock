121d9ad34e86408a0be49f7d5c69066d4e2a504c
sepseel <sepsel64@gmail.com>
2020-08-26 17:24:24 +0200

    added patches

--- config.def.h
+++ config.def.h
@@ -10,3 +10,24 @@ static const char *colorname[NUMCOLS] = {
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/* default message */
+static const char * message = "Suckless: Software that sucks less.";
+
+/* text color */
+static const char * text_color = "#ffffff";
+
+/* text size (must be a valid size) */
+static const char * font_name = "6x10";
+
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 5;
+
+/*Enable blur*/
+#define BLUR
+/*Set blur radius*/
+static const int blurRadius=5;
+/*Enable Pixelation*/
+//#define PIXELATION
+/*Set pixelation radius*/
+static const int pixelSize=0;
