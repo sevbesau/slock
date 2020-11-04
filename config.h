aac1bd7357f496cdf84a986f05b10bf3bb1fdc68
sepseel <sepsel64@gmail.com>
2020-11-04 01:16:14 +0100

    longer monitor timeout

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static const char *colorname[NUMCOLS] = {
 static const int failonclear = 1;
 
 /* time in seconds before the monitor shuts down */
-static const int monitortime = 5;
+static const int monitortime = 60;
 
 // Icon to draw in the center
 static const char * icon_path = "/home/sepseel/images/icon.png";
