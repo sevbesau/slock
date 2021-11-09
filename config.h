c3ae594e5fcf17b390661c0f78569d19e121a01f
sevbesau <seppe@sevbesau.xyz>
2021-11-09 04:07:50 +0100

    removed symlink

--- config.h
+++ config.h
@@ -1,1 +1,23 @@
-/home/sevbesau/.cache/wal/config.h
+/* user and group to drop privileges to */
+static const char *user = "sevbesau";
+static const char *group = "sevbesau";
+
+#include "/home/sevbesau/.cache/wal/colors-wal-slock.h"
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
+
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 60;
+
+// Icon to draw in the center
+static const char *icon_path = "/home/sevbesau/images/assets/lock.png";
+
+/*Enable blur*/
+#define BLUR
+/*Set blur radius*/
+static const int blurRadius = 10;
+/*Enable Pixelation*/
+//#define PIXELATION
+/*Set pixelation radius*/
+static const int pixelSize = 0;
